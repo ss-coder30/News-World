e773a10ac743f23f265954a74e1335f5a3bece3e
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2023-03-23 18:59:09 +0530

    api added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const path = require('path')
+const { join } = require('path')
+const axios = require('axios')
+
 const app = express()
 const port = 2000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/api', async(req, res) => {
+    console.log(req._parsedUrl.query)
+    let r = await axios("https://newsapi.org/v2/everything?"+req._parsedUrl.query)
+    let a = r.data
+    res.json(a)
 })
 
 app.listen(port, () => {
