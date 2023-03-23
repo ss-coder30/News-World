1048479fb7bf81830fc96f221d058d4510c64bdd
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2023-03-23 19:18:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios')
 const app = express()
 const port = 2000
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html")
 })
 
